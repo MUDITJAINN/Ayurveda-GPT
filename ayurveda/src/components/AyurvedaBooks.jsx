ba63e0540b3e4ac6f2d1e33bfa91940b3556b925
albinsabu2023 <albinsabu2023@gmail.com>
2024-10-05 01:11:09 +0530

    updated books section

--- ayurveda/src/components/AyurvedaBooks.jsx
+++ ayurveda/src/components/AyurvedaBooks.jsx
@@ -3,52 +3,52 @@ import styles from "../styles/App.module.css";
 function AyurvedaBooks() {
   const data = [
     {
-      title: "Ayurveda and the Mind by",
-      author: "Dr. David Frawley",
-      img: "ayuim1.jpg",
+      title: "The Ayurvedic Pharmacopoeia Of INDIA",
+      author: "Ministry of Family Health And Welfare ,New Delhi",
+      img: "book1.png",
       description:
-        "After Vasant Lad, David Frawley is seen as one of the most knowledgeable Western experts on Ayurveda. This book takes a more in-depth look at Ayurveda and focuses on the physical, mental and emotional elements of it.",
-      link: "https://www.amazon.in/Ayurveda-Mind-Consciousness-David-Frawley/dp/812082010X",
+        "Though Ayurveda has under gone many changes in the course of its long history, it still remains the mainstay of medical relief to a large section of population of the nation. Due to urbanization and dwindling of forests, the Vaidya by and large is no longer a self contained unit collecting and preparing his own medicines as before.",
+      link: "https://drive.google.com/file/d/1Nt1DNaKjI214ml918b2hrHbYoCrj4dog/view?usp=sharing",
     },
     {
-      title: "Ayurveda and Marma Therapy",
-      author: "Dr. David Frawley, Dr. Subhash Ranade and Dr. Avinash Lele",
-      img: "ayuimg2.jpg",
+      title: "Materia Medica of Ayurveda",
+      author: "Vaidhya Bhagwan Dash",
+      img: "book2.png",
       description:
-        "For those who have a background in massage or acupuncture, or a strong interest in how the body responds to physical touch, this is a wonderful book to use for self-care and also when helping others",
-      link: "https://www.amazon.in/Ayurveda-Marma-Therapy-Energy-Healing/dp/0940985594",
+        "This book explains the Every details of Ayurveda in 20 different chapters written by Vaidhya Bhagwan Dash available in English ",
+      link: "https://drive.google.com/file/d/1CzHx_IvvNJup6NImp7VqKupbvY4Ctzai/view?usp=sharing",
     },
     {
-      title: "The Everyday Ayurveda Cookbook ",
-      author: "Kate O’Donnell",
-      img: "ayuim3.jpg",
+      title: "Ayurveda Ancient Wisdom for a Modern Lifestyle ",
+      author: "Dr. Suhas Kshirsagar",
+      img: "book3.png",
       description:
-        "There are a growing number of Ayurvedic recipe books available these days, and this one is particularly easy to follow and understand, as it has a strong Western influence.",
-      link: "https://www.amazon.in/Everyday-Ayurveda-Cookbook-Seasonal-Eating/dp/1611802296",
+        "In this Book, we’ll delve into the principles of Ayurveda, a Sanskrit word that translates to “the science of life.” We’ll also look at imbalances in the body/mind/spirit and how to use natural methods to gently move them back into balance..",
+      link: "https://drive.google.com/file/d/1zN84MeER-BQITMvgP-pDyXLlUXsah3Oz/view?usp=sharing",
     },
     {
-      title: "The Complete Book of Ayurvedic Home Remedies:",
-      author: "Vasant Lad",
-      img: "ayuim4.jpg",
+      title: "Classical Ayurvedic Prescriptions For Common Diseases",
+      author: "Dr M.M Padhi",
+      img: "book4.png",
       description:
-        "A detailed and thorough book, including diet and lifestyle guidelines, as well as seasonal advice. Tips are given to suit each person’s dosha (mind and body type), and a large portion of the book is dedicated to the Illnesses and Remedies A-Z section",
-      link: "https://www.amazon.in/Complete-Book-Ayurvedic-Home-Remedies/dp/0749927658",
+        "This hand book enriched with multiple prescription option from classical texts, which are freely available in the market being manufactured by various companies and easily adopted by general practitioners in rural and urban India",
+      link: "https://lib.iim.cmb.ac.lk/wp-content/uploads/2019/04/Classical-Ayurvedic-Prescriptions-for-common-diseases.pdf",
     },
     {
-      title: "Practical Ayurveda ",
-      author: " Sivananda Yoga Vedanta Centre",
-      img: "ayuim5.jpg",
+      title: "Ayurvedic Science Of Food and Nutrition ",
+      author: " Sanjeev Rastogi ",
+      img: "book5.png",
       description:
-        "Discover your Ayurvedic body type through a simple self-assessment questionnaire, and how to apply ayurvedic practises to elements of any lifestyle. Whether it's adjusting your diet to suit your body type; energising your body through yoga; or relaxing your mind with meditation and positive thinking exercises, this all-encompassing health book has something for everyone to love.",
-      link: "https://www.amazon.in/Practical-Ayurveda-Find-Bring-Balance/dp/0241302129",
+        "For centuries Ayurveda has emphasized the role of food in health and disease. Modern medicine started out by basing all its treatments on drug-based principles and focused on specifi c problems rather than on individuals and the food they ate. This book satisfies a long-felt need of many physicians and nutritionists who were aware of the immense knowledge that existed but never knew how to access that information",
+      link: "https://lib.iim.cmb.ac.lk/wp-content/uploads/2019/04/Ayurvedic-Science-of-Food-and-Nutrition-PDFDrive.com-.pdf",
     },
     {
-      title: "Everyday Ayurveda  ",
-      author: " Dr Bhaswati Bhattacharya",
-      img: "ayuim6.jpg",
+      title: "Everyday Ayurveda   ",
+      author: " Danny Cavanagh & Carol Willis",
+      img: "book6.png",
       description:
-        "Taking us through a day in the life of Ayurveda living, Dr Bhaswati Bhattacharya illustrates the core principles of Ayurveda and shows us how to incorporate these in our routine. She explains the logic behind the changes she recommends and how they benefit us. Informative and accessible, Everyday Ayurveda is the perfect lifestyle",
-      link: "https://www.amazon.in/Everyday-Ayurveda-Daily-Habits-Change/dp/8184004575",
+        "This book has been compiled to provide a practical help guide to those interested in following a healthy Ayurvedic lifestyle. With complementary therapies and esoteric philosophies becoming much more widely accepted and available in the west many people are interested in finding out more. ",
+      link: "https://www.ayurveda.uk.com/s/Ch-1-10-Everyday-Ayurveda-book.pdf",
     },
   ];
   return (
